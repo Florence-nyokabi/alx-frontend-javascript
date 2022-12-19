fa8c20a84d99d5285b780b49a94f4dad076a6581
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-12-19 14:10:26 +0300

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disable-next-line
   for (const value of array) {
-    value = appendString + value;
-}
-  
+    // eslint-disable-next-line
+    array[array.indexOf(value)] = appendString + value;
+  }
+
   return array;
 }
-  
