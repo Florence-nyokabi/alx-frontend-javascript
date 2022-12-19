1fc46b62df300fe284737338b90405e4c48ff838
Florence-wangui <wanguif018@gmail.com>
2022-12-19 12:18:04 +0300

    Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,11 +2,14 @@ export function taskFirst() {
     const task = 'I prefer const when I can.'; // declare variable task using `const`
     return task;
 }
+
 export function getLast() {
     return ' is okay';
 }
+
 export function taskNext() {
     let combination = 'But sometimes let'; // declare variable combination using `let`
     combination += getLast();
+
     return combination;
 }
